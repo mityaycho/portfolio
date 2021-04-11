d8be9eb9ce485d2aa1060b1efa87fa077575a8ed
mityaycho <mityaycho@gmail.com>
2021-04-11 21:30:16 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ class Star {
     this.px = this.x;
     this.py = this.y;
     this.z += speed;
-    this.x += this.x*(speed*0.2)*this.z;
+    this.x += this.x * (speed * 0.2) * this.z;
     this.y += this.y * (speed * 0.2) * this.z;
     if (this.x > canvas.width / 2 + 50 || this.x < -canvas.width / 2 - 50 || this.y > canvas.height / 2 + 50 || this.y < -canvas.height / 2 - 50) {
       this.x = Math.random() * canvas.width - canvas.width / 2;
